7d3807edf2b02278a7674c0998600f7897665b5a
MiaMaria22 <marialyrberg@hotmail.com>
2023-05-29 13:38:08 +0200

    Databas

---
+++ Lottospelet/DataBas/ISqliteDataAccess.cs
@@ -0,0 +1,9 @@
+﻿namespace Lottospelet.DataBas
+{
+	internal interface ISqliteDataAccess
+	{
+		List<T> LoadData<T>(string sql);
+		void SaveData<T>(T person, string sql);
+		void UpdateData<T>(T person, string sql);
+	}
+}
---
+++ Lottospelet/DataBas/SqliteDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottospelet.DataBas
+{
+	internal class SqliteDataAccess : ISqliteDataAccess
+	{
+		public List<T> LoadData<T>(string sql)
+		{
+			using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+			{
+				var output = cnn.Query<T>(sql, new DynamicParameters());
+				return output.ToList();
+			}
+		}
+
+		public void SaveData<T>(T person, string sql)
+		{
+			using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+			{
+				cnn.Execute(sql, person);
+			}
+		}
+		public void UpdateData<T>(T person, string sql)
+		{
+			using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+			{
+				cnn.Execute(sql, person);
+			}
+		}
+
+		private string LoadConnectionString(string id = "Default")
+		{
+			return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+		}
+	}
+}
