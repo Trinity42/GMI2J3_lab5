1dfdbb9d0fc1e0d45916cace402d627f07704d4e
Therese Lissel <therese.lissel@gmail.com>
2023-05-29 12:24:21 +0200

    först commit

---
+++ Lottospelet/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Lottospelet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = ContainerConfig.Configure();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+                app.Run();
+            }
+
+        }
+    }
+}
