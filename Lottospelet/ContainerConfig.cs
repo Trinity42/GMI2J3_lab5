436f53e80bba31af6f69648faac9d93ae717e7c9
Therese Lissel <therese.lissel@gmail.com>
2023-05-29 12:47:34 +0200

    containercofig

---
+++ Lottospelet/ContainerConfig.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottospelet
+{
+    public class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Application>().As<IApplication>();
+            builder.RegisterType<externalIDTjänst>().As<IexternalIDTjänst>();
+            builder.RegisterType<ViTestarOmDettaFungerar>().As<IViTestarOmDettaFungerar>();
+            builder.RegisterType<PersonProcessor>().As<IPersonProcessor>();
+            builder.RegisterType<SqliteDataAccess>().As<ISqlitDataAccess>();
+            builder.RegisterType<Lottorad>().As<ILottorad>();
+
+
+            return builder.Build();
+        }
+    }
+}
