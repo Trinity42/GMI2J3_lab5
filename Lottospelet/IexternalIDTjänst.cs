ec1b76e8094d18bfe475771bfc96556e59d8fb6d
MiaMaria22 <marialyrberg@hotmail.com>
2023-05-29 12:50:57 +0200

    Idtjänst

---
+++ Lottospelet/IexternalIDTjänst.cs
@@ -0,0 +1,7 @@
+﻿namespace Lottospelet
+{
+	public interface IexternalIDTjänst
+	{
+		bool Authenticate(string username, string password);
+	}
+}
---
+++ Lottospelet/externalIDTjänst.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottospelet
+{
+	public class externalIDTjänst : IexternalIDTjänst
+	{
+		public bool Authenticate(string username, string password)
+		{
+			bool isRight = false;
+			if (username == "LifeSux" && password == "BraLösenOrd")
+				isRight = true;
+			return isRight;
+		}
+	}
+}
